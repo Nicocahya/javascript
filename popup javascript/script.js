eabf66803eabc0e17cd47efc7372b2ae9bb7c424
Nicocahya <nr016082@gmail.com>
2024-02-15 08:30:06 +0700

    popup box

---
+++ popup javascript/script.js
@@ -0,0 +1,22 @@
+// EXTERNAL
+
+// var nama = prompt('masukkan nama anda:');
+// alert(nama);
+
+// var tes = confirm('anda yakin?');
+// if( tes === true ) {
+//     alert('user menekan OK!');
+// } else {
+//     alert('user menekan CENCEL');
+// }
+
+alert('Wellcome!');
+var lagi = true;
+
+while (lagi === true) {
+  var nama = prompt('masukkan nama');
+  alert('hello ' + nama);
+
+  lagi = confirm('coba lagi?');
+}
+alert('terima kasih');
