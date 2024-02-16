cb2df107af0bb51baccb47080b09034daa9df04c
Nicocahya <nr016082@gmail.com>
2024-02-16 10:17:00 +0700

    game suit

--- membeuat game suit/script.js
+++ membeuat game suit/script.js
@@ -1,34 +1,34 @@
 var tanya = true;
 while (tanya) {
   // PILIHAN PLAYER
-  var p = prompt('pilih : batu, gunting, kertas');
+  var p = prompt('pilih : orang, semut, gajah');
   // PILIHAN COMP
   // COMP MEMILIH RANDOM
   var comp = Math.random();
 
   if (comp < 0.34) {
-    comp = 'batu';
+    comp = 'semut';
   } else if (comp >= 0.34 && comp < 0.67) {
-    comp = 'gunting';
+    comp = 'orang';
   } else {
-    comp = 'kertas';
+    comp = 'gajah';
   }
 
   var hasil = '';
   // MENENTUKAN RULES
   if (p == comp) {
     hasil = 'SERI';
-  } else if (p == 'kertas') {
+  } else if (p == 'gajah') {
     // if( comp == 'gajah' ) {
     //     hasil = 'KALAH!';
     // } else {
     //     hasil = 'MENANG!';
     // }
-    hasil = comp == 'batu' ? 'MENANG!' : 'KALAH!';
-  } else if (p == 'gunting') {
-    hasil = (comp = 'batu') ? 'KALAH!' : 'MENANG!';
-  } else if (p == 'batu') {
-    hasil = (comp = 'kertas') ? 'KALAH!' : 'MENANG!';
+    hasil = comp == 'orang' ? 'MENANG!' : 'KALAH!';
+  } else if (p == 'gajah') {
+    hasil = (comp = 'orang') ? 'KALAH!' : 'MENANG!';
+  } else if (p == 'semut') {
+    hasil = (comp = 'semut') ? 'KALAH!' : 'MENANG!';
   } else {
     hasil = 'memasukkan pilihan yang salah';
   }
